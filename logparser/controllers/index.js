4cf7a1068ba8237fc07fbdfcbb504ae9789928fc
Dipak <dipakbhise420@gmail.com>
2023-02-01 09:31:11 +0530

    added validation for wrong file format

--- logparser/controllers/index.js
+++ logparser/controllers/index.js
@@ -7,25 +7,32 @@ module.exports.logParser = async (req, res, next) => {
 
   const logs = fileContent.split("\n"); // split file date with \n
   //   console.log("logs", logs);
-  const parsedLogs = []; // to store final object of file data
+  let parsedLogs = []; // to store final object of file data
 
   logs.forEach((log) => {
     if (log) {
       const [timestamp, level, detailsJSON] = log.split(" - "); // separate timestamp, level and object from file data
       //   console.log("detailsJSON", detailsJSON);
-      const { transactionId, err, userId, code, user } =
-        JSON.parse(detailsJSON); // separate transactionId, err, userId, code, user from object
 
-      // push formated data in 'parsedLogs' in array
-      parsedLogs.push({
-        timestamp: timestamp ? new Date(timestamp).getTime() : 0,
-        loglevel: level ? level : 0,
-        transactionId: transactionId ? transactionId : 0,
-        err: err ? err : 0,
-        userId: userId ? userId : 0,
-        code: code ? code : 0,
-        user: user ? user : 0,
-      });
+      if ((timestamp, level, detailsJSON)) {
+        const { transactionId, err, userId, code, user } =
+          JSON.parse(detailsJSON); // separate transactionId, err, userId, code, user from object
+
+        // push formated data in 'parsedLogs' in array
+        parsedLogs.push({
+          timestamp: timestamp ? new Date(timestamp).getTime() : 0,
+          loglevel: level ? level : 0,
+          transactionId: transactionId ? transactionId : 0,
+          err: err ? err : 0,
+          userId: userId ? userId : 0,
+          code: code ? code : 0,
+          user: user ? user : 0,
+        });
+      }
+    } else {
+      if (parsedLogs.length == 0) {
+        parsedLogs = [];
+      }
     }
   });
 
@@ -33,5 +40,9 @@ module.exports.logParser = async (req, res, next) => {
 
   // send formated data to frontend
 
-  res.status(200).send({ parsedLogs: parsedLogs });
+  if (parsedLogs.length > 0) {
+    res.status(200).send({ parsedLogs: parsedLogs });
+  } else {
+    res.status(400).send({ msg: "Please Select valid file content" });
+  }
 };
